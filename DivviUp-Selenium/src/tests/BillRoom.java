6c32bd9914807c8a838d0f0f2db34469610f3d18
lauragarling <44353212+lauragarling@users.noreply.github.com>
2021-04-09 20:05:41 -0500

    Add files via upload

--- DivviUp-Selenium/src/tests/BillRoom.java
+++ DivviUp-Selenium/src/tests/BillRoom.java
@@ -1,31 +1,143 @@
 package tests;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 // this class includes interaction between billroompayer and billroomnonpayer
 public class BillRoom {
-	WebDriver driver;
+	WebDriver driver1;
+	WebDriver driver2;
 	
 	public void launchBrowser() {
 		System.setProperty("webdriver.chrome.driver", "/Users/lauragarling/Desktop/Selenium/chromedriver");
-		driver = new ChromeDriver();
-		driver.get("http://localhost:3000/billroomnonpayer/H4000");
+		driver1 = new ChromeDriver();
+		driver1.get("http://localhost:3000/home");
+		
+		driver2 = new ChromeDriver();
+		driver2.get("http://localhost:3000/home");
+		
 	}
 
 	public void closeBrowser() {
-		driver.quit();
+		driver1.quit();
+		driver2.quit();
 	}
 	
+	public void testAdd() throws InterruptedException {
+		
+		Thread.sleep(3000);
+		if (driver1.getCurrentUrl().equals("http://localhost:3000/start")) { // logs in if previous cookie DNE
+			driver1.findElement(By.name("loginbutton")).click();
+			Thread.sleep(2000);
+			driver1.findElement(By.id("exampleInputEmail1")).sendKeys("garling@wisc.edu");
+			driver1.findElement(By.id("exampleInputPassword1")).sendKeys("owen");
+			driver1.findElement(By.name("submit")).click();
+			Thread.sleep(2000);
+		}
+		
+		Thread.sleep(3000);
+		if (driver2.getCurrentUrl().equals("http://localhost:3000/start")) { // logs in if previous cookie DNE
+			driver2.findElement(By.name("loginbutton")).click();
+			Thread.sleep(2000);
+			driver2.findElement(By.id("exampleInputEmail1")).sendKeys("owolman@wisc.edu");
+			driver2.findElement(By.id("exampleInputPassword1")).sendKeys("lizard");
+			driver2.findElement(By.name("submit")).click();
+			Thread.sleep(2000);
+		}
+		
+		Thread.sleep(3000);
+		
+		driver1.navigate().to("http://localhost:3000/billroompayer/H4000");
+		driver2.navigate().to("http://localhost:3000/billroomnonpayer/H4000");
+		
+		Thread.sleep(3000);
+		
+		driver1.findElement(By.name("add")).click();
+		driver1.findElement(By.name("itname")).sendKeys("pop");
+		driver1.findElement(By.name("cost")).sendKeys("3.00");
+		driver1.findElement(By.name("butt")).click();
+		
+		Thread.sleep(3000);
+		
+		driver2.navigate().refresh();
+
+		Thread.sleep(3000);
+		
+		String name1 = driver1.findElement(By.name("name")).getText();
+		String name2 = driver2.findElement(By.name("name1")).getText();
+		Assert.assertEquals(name1, name2);
+		
+		System.out.println("Test 1 of 4: PASSED");
+	}
 	
+	public void testDuplicate() throws InterruptedException {
+		
+		driver1.findElement(By.name("dup")).click();
+		
+		Thread.sleep(3000);
+		
+		driver2.navigate().refresh();
+
+		Thread.sleep(3000);
+		
+		String name1 = driver1.findElement(By.name("name")).getText();
+		String name2 = driver2.findElement(By.name("name1")).getText();
+		Assert.assertEquals(name1, name2);
+		
+		System.out.println("Test 2 of 4: PASSED");		
+	}
+	
+	public void testDelete() throws InterruptedException {
+		
+		driver1.findElement(By.name("del")).click();
+		
+		Thread.sleep(3000);
+		
+		driver2.navigate().refresh();
+
+		Thread.sleep(3000);
+		
+		String name1 = driver1.findElement(By.name("name")).getText();
+		String name2 = driver2.findElement(By.name("name1")).getText();
+		Assert.assertEquals(name1, name2);
+		
+		System.out.println("Test 3 of 4: PASSED");		
+	}
+	
+	public void testEdit() throws InterruptedException {
+		
+		driver1.findElement(By.name("e")).click();
+		driver1.findElement(By.name("ename")).sendKeys(Keys.BACK_SPACE);
+		driver1.findElement(By.name("ename")).sendKeys(Keys.BACK_SPACE);
+		driver1.findElement(By.name("ename")).sendKeys(Keys.BACK_SPACE);
+		driver1.findElement(By.name("ename")).sendKeys("burger");
+		driver1.findElement(By.name("up")).click();
+
+		Thread.sleep(3000);
+		
+		String name1 = driver1.findElement(By.name("name")).getText();
+		String name2 = driver2.findElement(By.name("name1")).getText();
+		Assert.assertEquals(name1, name2);
+		
+		String ActName = name1;
+		String ExpName = driver2.findElement(By.name("name1")).getText();
+		Assert.assertEquals(ExpName, ActName);
+		
+		System.out.println("Test 4 of 4: PASSED");		
+	}
 	
 	
 	public static void main(String[] args) throws InterruptedException {
 		BillRoom obj = new BillRoom();
 		obj.launchBrowser();
+		obj.testAdd();
+		obj.testDuplicate();
+		obj.testDelete();
+		obj.testEdit();
 		System.out.println("Tests have finished running.");
-		obj.closeBrowser();
+		//obj.closeBrowser();
 	}
 
 }
--- DivviUp-Selenium/src/tests/HomePage.java
+++ DivviUp-Selenium/src/tests/HomePage.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-// need to figure out how to test popups
-// Create Room, Join room, and PastBills not tested 
+//NEED: Join with blank string 
+// NEED: test Past Bills 
 
 public class HomePage {
 	WebDriver driver;
@@ -16,12 +16,10 @@ public class HomePage {
 		driver.get("http://localhost:3000/home");
 	}
 	
-	// can't figure out how to test popups
-	/*
+	
 	public void emptyRoomName() throws InterruptedException {
 		Thread.sleep(3000);
 		if (driver.getCurrentUrl().equals("http://localhost:3000/start")) { // logs in if previous cookie DNE
-			System.out.print("here");
 			driver.findElement(By.name("loginbutton")).click();
 			Thread.sleep(2000);
 			driver.findElement(By.id("exampleInputEmail1")).sendKeys("garling@wisc.edu");
@@ -32,10 +30,10 @@ public class HomePage {
 		
 		Thread.sleep(2000);
 		driver.findElement(By.name("create")).click();
-		driver.switchTo().activeElement();
-		driver.findElement(By.className("box1")).click();
-		driver.findElement(By.className("box1")).sendKeys("");
-		driver.findElement(By.className("create2")).click();
+		//driver.switchTo().activeElement();
+		driver.findElement(By.name("box1")).click();
+		driver.findElement(By.name("box1")).sendKeys("");
+		driver.findElement(By.name("create2")).click();
 		
 		String ActualErr = driver.findElement(By.name("error")).getText();
 		String ExpectedErr = "Need a room name";
@@ -44,10 +42,106 @@ public class HomePage {
 		driver.navigate().refresh();
 		Thread.sleep(2000);
 		
-		System.out.print("Test 1 of 1: PASSED");
+		System.out.println("Test 1 of 5: PASSED");
 
 	}
-	*/
+	
+	public void createValidRoom() throws InterruptedException {
+		Thread.sleep(3000);
+		if (driver.getCurrentUrl().equals("http://localhost:3000/start")) { // logs in if previous cookie DNE
+			driver.findElement(By.name("loginbutton")).click();
+			Thread.sleep(2000);
+			driver.findElement(By.id("exampleInputEmail1")).sendKeys("garling@wisc.edu");
+			driver.findElement(By.id("exampleInputPassword1")).sendKeys("owen");
+			driver.findElement(By.name("submit")).click();
+			Thread.sleep(2000);
+		}
+		
+		Thread.sleep(3000);
+		driver.findElement(By.name("create")).click();
+		//driver.switchTo().activeElement();
+		driver.findElement(By.name("box1")).click();
+		driver.findElement(By.name("box1")).sendKeys("Preparing for Botanic Panic");
+		driver.findElement(By.name("create2")).click();
+		
+		Thread.sleep(3000);
+		
+		if (!driver.getCurrentUrl().contains("http://localhost:3000/billroompayer/")) {
+			System.out.println("FAIL: New room not generated.");
+		}
+		
+		System.out.println("Test 2 of 5: PASSED");
+		
+		driver.navigate().back();
+		
+		
+	}
+	
+	public void joinValidRoom() throws InterruptedException {
+		Thread.sleep(3000);
+		if (driver.getCurrentUrl().equals("http://localhost:3000/start")) { // logs in if previous cookie DNE
+			driver.findElement(By.name("loginbutton")).click();
+			Thread.sleep(2000);
+			driver.findElement(By.id("exampleInputEmail1")).sendKeys("garling@wisc.edu");
+			driver.findElement(By.id("exampleInputPassword1")).sendKeys("owen");
+			driver.findElement(By.name("submit")).click();
+			Thread.sleep(2000);
+		}
+		
+		Thread.sleep(2000);
+		driver.findElement(By.name("join")).click();
+		driver.findElement(By.name("box1")).click();
+		driver.findElement(By.name("box1")).sendKeys("45000");
+		driver.findElement(By.name("join2")).click();
+		Thread.sleep(3000);
+		
+		String ActualURL = driver.getCurrentUrl();
+		String ExpectedURL = "http://localhost:3000/billroomnonpayer/45000";
+		Assert.assertEquals(ExpectedURL, ActualURL);
+		
+		Thread.sleep(3000);
+		String ActualTxt = driver.findElement(By.name("code")).getText();
+		String ExpectedTxt = "Room Code: 45000";
+		Assert.assertEquals(ExpectedTxt, ActualTxt);
+		
+		System.out.println("Test 3 of 5: PASSED");
+
+		driver.navigate().back();
+		
+	}
+	
+	public void invalidRoomCode() throws InterruptedException {
+		Thread.sleep(3000);
+		if (driver.getCurrentUrl().equals("http://localhost:3000/start")) { // logs in if previous cookie DNE
+			driver.findElement(By.name("loginbutton")).click();
+			Thread.sleep(2000);
+			driver.findElement(By.id("exampleInputEmail1")).sendKeys("garling@wisc.edu");
+			driver.findElement(By.id("exampleInputPassword1")).sendKeys("owen");
+			driver.findElement(By.name("submit")).click();
+			Thread.sleep(2000);
+		}
+		
+		Thread.sleep(2000);
+		driver.findElement(By.name("join")).click();
+		driver.findElement(By.name("box1")).click();
+		driver.findElement(By.name("box1")).sendKeys("XXXXX");
+		driver.findElement(By.name("join2")).click();
+		Thread.sleep(3000);
+		
+		String ActualURL = driver.getCurrentUrl();
+		String ExpectedURL = "http://localhost:3000/home";
+		Assert.assertEquals(ExpectedURL, ActualURL);
+		
+		Thread.sleep(3000);
+		String ActualErr = driver.findElement(By.name("error")).getText();
+		String ExpectedErr = "Room does not exist, enter valid code";
+		Assert.assertEquals(ExpectedErr, ActualErr);
+		
+		System.out.println("Test 4 of 5: PASSED");
+
+		driver.navigate().back();
+	}
+	
 	
 	public void logout() throws InterruptedException {
 		Thread.sleep(3000);
@@ -67,7 +161,7 @@ public class HomePage {
 		String ExpectedURL = "http://localhost:3000/start";
 		Assert.assertEquals(ExpectedURL, ActualURL);
 
-		System.out.println("Test 1 of 1: PASSED");
+		System.out.println("Test 5 of 5: PASSED");
 		
 	}
 	
@@ -78,7 +172,10 @@ public class HomePage {
 	public static void main(String[] args) throws InterruptedException {
 		HomePage obj = new HomePage();
 		obj.launchBrowser();
-		//obj.emptyRoomName();
+		obj.emptyRoomName();
+		obj.createValidRoom();
+		obj.joinValidRoom();
+		obj.invalidRoomCode();
 		obj.logout();
 		System.out.println("Tests have finished running.");
 		obj.closeBrowser();
